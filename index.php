2a33e2708867d0e70e85f25997628a30fbf3ff23
Luca <lucaciprani1@gmail.com>
2021-10-07 16:13:39 +0200

    end exer

--- index.php
+++ index.php
@@ -2,8 +2,20 @@
 $parag = 'Lorem ipsum dolor sit amet consectetur adipisicing elit ipsum. Porro aspernatur fuga eaque ipsum nesciunt, earum, natus commodi ipsum, cumque distinctio dicta? Asperiores assumenda praesentium commodi esse ipsum voluptatibus ad nam ut?';
 $paragLng = strlen($parag);
 $badWord = $_GET['badword'];
-$newParag = str_replace('ipsum', ' ***', $parag);
+$replacement = '***';
+$newParag = str_replace($badWord, $replacement, $parag);
+$newparagLng = strlen($newParag);
 ?>
+
+<div class="container">
+    <h1>Titolo paragrafo </h1>
+    <p> <?php echo $parag ?> </p>
+    <h3>Paragraph length is <?php echo $paragLng ?> words </h3>
+    <h3> Badword is: <span> <?php echo $badWord ?> </span> </h3>
+
+    <p> <?php echo $newParag ?> </p>
+    <h3>Paragraph length is <?php echo $newparagLng ?> words </h3>
+</div>
 
 <style>
     body{
@@ -21,13 +33,3 @@ $newParag = str_replace('ipsum', ' ***', $parag);
         color: blue;
     }
 </style>
-
-<div class="container">
-    <h1>Titolo paragrafo </h1>
-    <p> <?php echo $parag ?> </p>
-    <h3>Paragraph length is <?php echo $paragLng ?> words </h3>
-    <h3> Badword is: <span> <?php echo $badWord ?> </span> </h3>
-
-    <p> <?php echo $newParag ?> </p>
-    <h3>Paragraph length is <?php echo $paragLng ?> words </h3>
-</div>
